d0b649d77969758f4296f62a58bb7f5be5f247d4
nightbreeze6846 <gabriel0218@naver.com>
2015-04-29 16:56:35 +0900

    commit for base

---
+++ src/TPackage/main.java
@@ -0,0 +1,13 @@
+package TPackage;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hellow Testing for CI server!!");
+		System.out.println("Thank you!");
+
+	}
+
+}
